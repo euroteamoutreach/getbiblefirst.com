4479ec2eecddc651f6505aaba8658fb1d346e797
Joshua Steele <joshukraine@gmail.com>
2016-04-11 17:14:37 +0300

    Add top nav bar

---
+++ source/javascripts/_nav.js
@@ -0,0 +1,17 @@
+var $ = require("jquery");
+
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+});
+
+
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,6 @@
 require('_animate');
 require('_copyright');
+require('_nav-scroll');
 require('_nav-transparent');
+require('_nav');
 require('_scroll');
