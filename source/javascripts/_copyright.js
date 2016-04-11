fb765caf72aff294e08422f7b68c6cbbdfaa9c1e
Joshua Steele <joshukraine@gmail.com>
2016-04-11 15:51:43 +0300

    Add footer

---
+++ source/javascripts/_copyright.js
@@ -0,0 +1,7 @@
+var $ = require("jquery");
+var moment = require("moment")
+
+$(function() {
+  var year = moment().format("YYYY");
+  $('.year').append(year)
+});
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,4 @@
 require('_animate');
+require('_copyright');
 require('_nav-transparent');
 require('_scroll');
