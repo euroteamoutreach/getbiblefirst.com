bd6020272bdf723b81c44c1c97affdb966411fbb
Joshua Steele <joshukraine@gmail.com>
2016-05-29 21:25:26 +0300

    Add Rspec configs.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,14 @@
+require "rspec"
+require "capybara/rspec"
+require "middleman-core"
+require "middleman-core/rack"
+require "middleman-s3_sync"
+require "middleman-pry"
+
+middleman_app = ::Middleman::Application.new
+
+Capybara.app = ::Middleman::Rack.new(middleman_app).to_app do
+  set :root, File.expand_path(File.join(File.dirname(__FILE__), ".."))
+  set :environment, :development
+  set :show_exceptions, false
+end
