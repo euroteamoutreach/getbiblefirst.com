61ca374c72968af5b6b0c1b4604211d8fe7d3d2c
Joshua Steele <joshukraine@gmail.com>
2017-07-03 21:08:23 +0300

    Update deploy configs for staging.

--- environments/staging.rb
+++ environments/staging.rb
@@ -3,8 +3,8 @@
 # https://github.com/fredjean/middleman-s3_sync
 activate :s3_sync do |s3|
   s3.bucket = "staging.getbiblefirst.com"
-  s3.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
-  s3.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+  s3.aws_access_key_id = ENV["ETO_AWS_ACCESS_KEY_ID"]
+  s3.aws_secret_access_key = ENV["ETO_AWS_SECRET_ACCESS_KEY"]
   s3.prefer_gzip = true
   s3.index_document = "index.html"
   s3.error_document = "404.html"
