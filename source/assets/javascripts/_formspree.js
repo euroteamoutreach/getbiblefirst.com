41281b87bf0ee52c9f5b71b16f8013940652e01a
Joshua Steele <joshukraine@gmail.com>
2020-12-29 15:29:05 +0200

    Tweak thank-you page route logic.

--- source/assets/javascripts/_formspree.js
+++ source/assets/javascripts/_formspree.js
@@ -1,12 +1,10 @@
-window.addEventListener("DOMContentLoaded", function() {
-
+window.addEventListener('DOMContentLoaded', function () {
   // get the form elements defined in your form HTML above
 
-  var form = document.getElementById("my-form");
-  var button = document.getElementById("my-form-button");
-  var status = document.getElementById("my-form-status");
-  const page = (document.getElementsByTagName('h1')[0].innerHTML == 'Contact')
-    ? "contact" : "purchase";
+  var form = document.getElementById('my-form');
+  var button = document.getElementById('my-form-button');
+  var status = document.getElementById('my-form-status');
+  const page = document.body.classList[0];
 
   // Success and Error functions for after the form is submitted
 
@@ -16,12 +14,12 @@ window.addEventListener("DOMContentLoaded", function() {
   }
 
   function error() {
-    status.innerHTML = "Oops! There was a problem. Please try again later.";
+    status.innerHTML = 'Oops! There was a problem. Please try again later.';
   }
 
   // handle the form submission event
 
-  form.addEventListener("submit", function(ev) {
+  form.addEventListener('submit', function (ev) {
     ev.preventDefault();
     var data = new FormData(form);
     ajax(form.method, form.action, data, success, error);
@@ -33,8 +31,8 @@ window.addEventListener("DOMContentLoaded", function() {
 function ajax(method, url, data, success, error) {
   var xhr = new XMLHttpRequest();
   xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function() {
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
     if (xhr.readyState !== XMLHttpRequest.DONE) return;
     if (xhr.status === 200) {
       success(xhr.response, xhr.responseType);
